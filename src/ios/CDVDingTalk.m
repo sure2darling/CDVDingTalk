30869808cef7ef6185b535bdb4c9f5a3e1347e70
jsure <524530156@qq.com>
2019-08-02 10:05:53 +0800

    init

--- src/ios/CDVDingTalk.m
+++ src/ios/CDVDingTalk.m
@@ -1,12 +1,18 @@
 /********* CDVDingTalk.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <DTShareKit/DTOpenKit.h>
 
 @interface CDVDingTalk : CDVPlugin {
   // Member variables go here.
 }
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)openDingTalk:(CDVInvokedUrlCommand*)command;
+- (void)shareTextToDingTalk:(CDVInvokedUrlCommand*)command;
+- (void)shareImageToDingTalk:(CDVInvokedUrlCommand*)command;
+- (void)shareWebToDingTalk:(CDVInvokedUrlCommand*)command;
+- (void)ssoWithDingTalk:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation CDVDingTalk
@@ -15,5 +21,111 @@
 {
     NSLog(@"消息测试-----");
 }
+
+
+- (void)registerAppDingTalk
+{
+   [DTOpenAPI registerApp:@"dingoak5hqhuvmpfhpnjvt"];
+}
+
+
+- (void)openDingTalk:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"openDingTalk: %@", @([DTOpenAPI openDingTalk]));
+}
+
+
+- (void)shareTextToDingTalk:(CDVInvokedUrlCommand*)command
+{
+    DTSendMessageToDingTalkReq *sendMessageReq = [[DTSendMessageToDingTalkReq alloc] init];
+
+    DTMediaMessage *mediaMessage = [[DTMediaMessage alloc] init];
+
+    DTMediaTextObject *textObject = [[DTMediaTextObject alloc] init];
+    textObject.text = @"Hi DingTalk.";
+
+    mediaMessage.mediaObject = textObject;
+    sendMessageReq.message = mediaMessage;
+
+    BOOL result = [DTOpenAPI sendReq:sendMessageReq];
+    if (result)
+    {
+        NSLog(@"Text 发送成功.");
+    }
+    else
+    {
+        NSLog(@"Text 发送失败.");
+    }
+}
+
+- (void)shareImageToDingTalk:(CDVInvokedUrlCommand*)command
+{
+    DTSendMessageToDingTalkReq *sendMessageReq = [[DTSendMessageToDingTalkReq alloc] init];
+
+    DTMediaMessage *mediaMessage = [[DTMediaMessage alloc] init];
+
+    DTMediaImageObject *imageObject = [[DTMediaImageObject alloc] init];
+    imageObject.imageData = [NSData data];
+    imageObject.imageURL = @"https://img.alicdn.com/tps/TB1dagdIpXXXXc5XVXXXXXXXXXX.jpg";
+
+    mediaMessage.mediaObject = imageObject;
+    sendMessageReq.message = mediaMessage;
+
+    BOOL result = [DTOpenAPI sendReq:sendMessageReq];
+    if (result)
+    {
+        NSLog(@"Image 发送成功.");
+    }
+    else
+    {
+        NSLog(@"Image 发送失败.");
+    }
+}
+
+- (void)shareWebToDingTalk:(CDVInvokedUrlCommand*)command
+{
+    DTSendMessageToDingTalkReq *sendMessageReq = [[DTSendMessageToDingTalkReq alloc] init];
+
+    DTMediaMessage *mediaMessage = [[DTMediaMessage alloc] init];
+    DTMediaWebObject *webObject = [[DTMediaWebObject alloc] init];
+    webObject.pageURL = @"http://www.dingtalk.com/";
+
+    mediaMessage.title = @"钉钉";
+
+    mediaMessage.thumbURL = @"https://static.dingtalk.com/media/lALOGp__Tnh4_120_120.png";
+
+    // Or Set a image data which less than 32K.
+    // mediaMessage.thumbData = UIImagePNGRepresentation([UIImage imageNamed:@"open_icon"]);
+
+    mediaMessage.messageDescription = @"钉钉，是一个工作方式。为企业量身打造统一办公通讯平台";
+    mediaMessage.mediaObject = webObject;
+    sendMessageReq.message = mediaMessage;
+
+    BOOL result = [DTOpenAPI sendReq:sendMessageReq];
+    if (result)
+    {
+        NSLog(@"Link 发送成功.");
+    }
+    else
+    {
+        NSLog(@"Link 发送失败.");
+    }
+}
+
+
+- (void)ssoWithDingTalk:(CDVInvokedUrlCommand*)command
+{
+    DTAuthorizeReq *authReq = [DTAuthorizeReq new];
+
+    authReq.bundleId = @"dingoak5hqhuvmpfhpnjvt";
+    BOOL result = [DTOpenAPI sendReq:authReq];
+    if (result) {
+        NSLog(@"授权登录 发送成功.");
+    }
+    else {
+        NSLog(@"授权登录 发送失败.");
+    }
+}
+
 
 @end
--- www/CDVDingTalk.js
+++ www/CDVDingTalk.js
@@ -4,6 +4,25 @@ var exports = {
     coolMethod : function (arg0, success, error) {
         exec(success, error, 'CDVDingTalk', 'coolMethod', [arg0]);
     },
+    registerAppDingTalk: function (arg0, success, error) {
+        exec(success, error, 'CDVDingTalk', 'registerAppDingTalk', [arg0]);
+    },
+
+    openDingTalk: function (arg0, success, error) {
+        exec(success, error, 'CDVDingTalk', 'openDingTalk', [arg0]);
+    },
+    shareTextToDingTalk: function (arg0, success, error) {
+        exec(success, error, 'CDVDingTalk', 'shareTextToDingTalk', [arg0]);
+    },
+    shareImageToDingTalk: function (arg0, success, error) {
+        exec(success, error, 'CDVDingTalk', 'shareImageToDingTalk', [arg0]);
+    },
+    shareWebToDingTalk: function (arg0, success, error) {
+        exec(success, error, 'CDVDingTalk', 'shareWebToDingTalk', [arg0]);
+    },
+    ssoWithDingTalk: function (arg0, success, error) {
+        exec(success, error, 'CDVDingTalk', 'ssoWithDingTalk', [arg0]);
+    },
 };
 
 module.exports = exports
